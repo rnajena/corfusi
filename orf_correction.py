521dec52881864ff4d0b669c7463be82fb5e0bfb
nu76fet <sandra.triebel@uni-jena.de>
2021-04-07 15:25:25 +0200

    test

--- orf_correction.py
+++ orf_correction.py
@@ -103,80 +103,80 @@ os.system('rm ' + outputdir + 'blastn/*')
 
 
 # ############## update assembly sequence ###############
-# up_down_all = []
-# for elem in candidates:
-#     ### extract up and downstream region ###
-#     start = int(elem[2].start)
-#     end = int(elem[2].end)
-#     upstream = elem[0].seq[start-t:start]
-#     downstream = elem[0].seq[end+1:end+t+1]
+up_down_all = []
+for elem in candidates:
+    ### extract up and downstream region ###
+    start = int(elem[2].start)
+    end = int(elem[2].end)
+    upstream = elem[0].seq[start-t:start]
+    downstream = elem[0].seq[end+1:end+t+1]
     
-#     ### check length of up and downstream region ###
-#     if len(upstream) == t and len(downstream) == t:
-#         ### create query ###
-#         os.system('touch query.fasta')
-#         os.system('echo ">upstream\n' + str(upstream) + '\n>downstream\n' + str(downstream) + '" >> query.fasta')
+    ### check length of up and downstream region ###
+    if len(upstream) == t and len(downstream) == t:
+        ### create query ###
+        os.system('touch query.fasta')
+        os.system('echo ">upstream\n' + str(upstream) + '\n>downstream\n' + str(downstream) + '" >> query.fasta')
 
-#         ### blastn ###
-#         os.system('blastn -task blastn -max_target_seqs 1 -outfmt 6 -query query.fasta -db ' + outputdir + 'hybrid_blastdb -out ' + outputdir + 'blastn/' + elem[1] + '_results.out')
-#         os.system('rm query.fasta')
+        ### blastn ###
+        os.system('blastn -task blastn -max_target_seqs 1 -outfmt 6 -query query.fasta -db ' + outputdir + 'hybrid_blastdb -out ' + outputdir + 'blastn/' + elem[1] + '_results.out')
+        os.system('rm query.fasta')
 
-#         results = open(outputdir + 'blastn/' + elem[1] + '_results.out', 'r').readlines()
+        results = open(outputdir + 'blastn/' + elem[1] + '_results.out', 'r').readlines()
         
-#         up_down_pair = [results[0].split('\t')]
-#         for line in results:
-#             if line[0:10] == 'downstream':
-#                 up_down_pair.append(line.split('\t'))
-#                 break
+        up_down_pair = [results[0].split('\t')]
+        for line in results:
+            if line[0:10] == 'downstream':
+                up_down_pair.append(line.split('\t'))
+                break
         
-#         ### continue if up and downstream region matched ###
-#         if len(up_down_pair) == 2 and up_down_pair[0][1] == up_down_pair[1][1] and int(up_down_pair[0][3]) == t and int(up_down_pair[1][3]) == t:
-#             k = up_down_pair[0][0:2] + list(map(float, up_down_pair[0][2:]))
-#             l = up_down_pair[1][0:2] + list(map(float, up_down_pair[1][2:]))
-#             up_down_all.append((k, l, elem))
+        ### continue if up and downstream region matched ###
+        if len(up_down_pair) == 2 and up_down_pair[0][1] == up_down_pair[1][1] and int(up_down_pair[0][3]) == t and int(up_down_pair[1][3]) == t:
+            k = up_down_pair[0][0:2] + list(map(float, up_down_pair[0][2:]))
+            l = up_down_pair[1][0:2] + list(map(float, up_down_pair[1][2:]))
+            up_down_all.append((k, l, elem))
             
-#         up_down_pair = []
+        up_down_pair = []
 
 
-# ### sort blast results by start position ###
-# up_down_all.sort(key=lambda x: x[0][8])
+### sort blast results by start position ###
+up_down_all.sort(key=lambda x: x[0][8])
 
-# count = 0
-# log = open(outputdir + prefix + '.logfile', 'w')
-# log.write('short-read assembly prokka id\tstart\tend\told sequence\tnew sequence\n')
-# for elem in up_down_all:
-#     h_start = int(elem[0][9]) + count
-#     h_end = int(elem[1][8]) + count
+count = 0
+log = open(outputdir + prefix + '.logfile', 'w')
+log.write('short-read assembly prokka id\tstart\tend\told sequence\tnew sequence\n')
+for elem in up_down_all:
+    h_start = int(elem[0][9]) + count
+    h_end = int(elem[1][8]) + count
 
-#     start = elem[2][2].start
-#     end = elem[2][2].end
+    start = elem[2][2].start
+    end = elem[2][2].end
 
-#     h_len = abs(h_end - h_start -1)
-#     sr_gene_len = end - start +1
+    h_len = abs(h_end - h_start -1)
+    sr_gene_len = end - start +1
 
-#     sr_gene = elem[2][0].seq[start:end+1]
+    sr_gene = elem[2][0].seq[start:end+1]
 
-#     ### if downstream < upstream: change variable and generate reverse complement ###
-#     if h_start > h_end:
-#         h_start, h_end = h_end, h_start
-#         sr_gene = sr_gene.reverse_complement()
+    ### if downstream < upstream: change variable and generate reverse complement ###
+    if h_start > h_end:
+        h_start, h_end = h_end, h_start
+        sr_gene = sr_gene.reverse_complement()
 
-#     ### filtering by length (20% longer or shorter than gene allowed) ###
-#     if abs(h_len - sr_gene_len) < sr_gene_len * 0.2:
-#         ### insert short-read gene in hybrid sequence ###
-#         hybrid_fasta[elem[0][1]].seq = hybrid_fasta[elem[0][1]].seq[:h_start+1] + sr_gene + hybrid_fasta[elem[0][1]].seq[h_end:]
+    ### filtering by length (20% longer or shorter than gene allowed) ###
+    if abs(h_len - sr_gene_len) < sr_gene_len * 0.2:
+        ### insert short-read gene in hybrid sequence ###
+        hybrid_fasta[elem[0][1]].seq = hybrid_fasta[elem[0][1]].seq[:h_start+1] + sr_gene + hybrid_fasta[elem[0][1]].seq[h_end:]
 
-#         ### update index count ###
-#         if sr_gene_len != h_len: count += sr_gene_len - h_len
+        ### update index count ###
+        if sr_gene_len != h_len: count += sr_gene_len - h_len
         
-#         ### write logfile ###
-#         log.write(elem[2][1] + '\t' + str(h_start+1) + '\t' + str(h_end-1) + '\t' + str(hybrid_fasta[elem[0][1]].seq[h_start+1:h_end]) + '\t' + str(sr_gene) + '\n')
+        ### write logfile ###
+        log.write(elem[2][1] + '\t' + str(h_start+1) + '\t' + str(h_end-1) + '\t' + str(hybrid_fasta[elem[0][1]].seq[h_start+1:h_end]) + '\t' + str(sr_gene) + '\n')
 
-# log.close()
+log.close()
 
-# os.system('rm -r ' + outputdir + 'blastn/')
-# os.system('rm ' + outputdir + 'hybrid_blastdb*')
+os.system('rm -r ' + outputdir + 'blastn/')
+os.system('rm ' + outputdir + 'hybrid_blastdb*')
 
-# ############## save new assembly ###############
-# with open(outputdir + prefix + '.fasta', 'w') as handle:
-#     SeqIO.write(hybrid_fasta.values(), handle, 'fasta')
+############## save new assembly ###############
+with open(outputdir + prefix + '.fasta', 'w') as handle:
+    SeqIO.write(hybrid_fasta.values(), handle, 'fasta')
